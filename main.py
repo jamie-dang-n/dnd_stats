ebc0978bde4fb60c6d8ea9802e1123d531299cfe
Jamie Dang <45217971+gothhouse@users.noreply.github.com>
2021-12-06 22:36:32 +0000

    Added cleric as an option, plus text to clarify available classes

--- main.py
+++ main.py
@@ -34,13 +34,23 @@ def suggestion(character_class, total_stats):
     constitution = "Constitution: " + str(total_stats[0])
     barbarian_string = ['The barbarian benefits aplenty from high constitution, and good strength.', constitution, strength, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
     return barbarian_string
+  elif character_class == "Bard" or character_class == "bard":
+    charisma = "Charisma: " + str(total_stats[0])
+    intelligence = "Intelligence: " + str(total_stats[1])
+    bard_string = ['The bard benefits aplenty from high charisma, and good intelligence.', charisma, intelligence, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return bard_string
+  elif character_class == "Cleric" or character_class == "cleric":
+    wisdom = "Wisdom: " + str(total_stats[0])
+    charisma = "Charisma: " + str(total_stats[1])
+    cleric_string = ['The cleric benefits aplenty from high wisdom, and good charisma.', wisdom, charisma, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return cleric_string
 
 sg.theme('DarkAmber')    # Keep things interesting for your users
 
 layout = [[sg.Text('Enter your class')],      
           [sg.Input(key='-IN-')],    
-          [sg.Text(key='-OUTPUT-')],
-          [sg.Text(key='-CLASSOUTFIRST-')],
+          [sg.Text('Available classes are bard, barbarian, and cleric', key='-OUTPUT-')],
+          [sg.Text( key='-CLASSOUTFIRST-')],
           [sg.Text(key='-CLASSOUTSEC-')],
           [sg.Text(key='-CLASSOUTTHIRD-')], 
           [sg.Text(key='-CLASSOUTFOUR-')],
