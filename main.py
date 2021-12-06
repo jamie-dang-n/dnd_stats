70c4ce327b3fcd6c7be1111b98824daeddfa4b25
Jamie Dang <45217971+gothhouse@users.noreply.github.com>
2021-12-06 22:52:37 +0000

    Completed all of the class options in the conditional, specifies strongest ability scores of each class

--- main.py
+++ main.py
@@ -30,20 +30,50 @@ def total_stats():
 
 def suggestion(character_class, total_stats):
   if character_class == "Barbarian":
-    strength = "Strength: " + str(total_stats[1])
-    constitution = "Constitution: " + str(total_stats[0])
-    barbarian_string = ['The barbarian benefits aplenty from high constitution, and good strength.', constitution, strength, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
-    return barbarian_string
+    first = "Strength: " + str(total_stats[1])
+    second = "Constitution: " + str(total_stats[0])
+    class_string = ['The barbarian benefits aplenty from high constitution, and good strength.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
   elif character_class == "Bard":
-    charisma = "Charisma: " + str(total_stats[0])
-    intelligence = "Intelligence: " + str(total_stats[1])
-    bard_string = ['The bard benefits aplenty from high charisma, and good intelligence.', charisma, intelligence, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
-    return bard_string
+    first = "Charisma: " + str(total_stats[0])
+    second = "Intelligence: " + str(total_stats[1])
+    class_string = ['The bard benefits aplenty from high charisma, and good intelligence.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
   elif character_class == "Cleric":
-    wisdom = "Wisdom: " + str(total_stats[0])
-    charisma = "Charisma: " + str(total_stats[1])
-    cleric_string = ['The cleric benefits aplenty from high wisdom, and good charisma.', wisdom, charisma, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
-    return cleric_string
+    first = "Wisdom: " + str(total_stats[0])
+    second = "Charisma: " + str(total_stats[1])
+    class_string = ['The cleric benefits aplenty from high wisdom, and good charisma.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Druid":
+    first = "Wisdom: " + str(total_stats[0])
+    second = "Dexterity: " + str(total_stats[1])
+    class_string = ['The druid benefits aplenty from high wisdom, and good dexterity.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Fighter":
+    first = "Strength: " + str(total_stats[0])
+    second = "Constitution: " + str(total_stats[1])
+    class_string = ['The fighter benefits aplenty from high strength, and good constitution.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Monk":
+    first = "Dexterity: " + str(total_stats[0])
+    second = "Wisdom: " + str(total_stats[1])
+    class_string = ['The monk benefits aplenty from high dexterity, and good wisdom.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Paladin":
+    first = "Charisma: " + str(total_stats[0])
+    second = "Strength: " + str(total_stats[1])
+    class_string = ['The paladin benefits aplenty from high charisma, and good strength.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Ranger":
+    first = "Charisma: " + str(total_stats[0])
+    second = "Strength: " + str(total_stats[1])
+    class_string = ['The ranger benefits aplenty from high strength, and good dexterity.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
+  elif character_class == "Rogue":
+    first = "Dexterity: " + str(total_stats[0])
+    second = "Charisma: " + str(total_stats[1])
+    class_string = ['The rogue benefits aplenty from high dexterity, and good charisma.', first, second, 'The rest of the scores may go wherever you please.', str(total_stats[2:])]
+    return class_string
 
 sg.theme('DarkAmber')    # Keep things interesting for your users
 
