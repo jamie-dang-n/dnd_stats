95599609f3eaf5c8a5b1c88b7e26ad091e1f5e80
Jamie Dang <45217971+gothhouse@users.noreply.github.com>
2022-01-24 22:10:04 +0000

    Added in Mr. W's suggestions: moved the "return class_string" after the if/elif/else statements and added an else error statement

--- stat_roller.py
+++ stat_roller.py
@@ -35,48 +35,42 @@ def suggestion(character_class, total_stats):
   if character_class == "Barbarian":
     first = "Strength: " + str(total_stats[0])
     second = "Constitution: " + str(total_stats[1])
-    class_string = ['The barbarian benefits aplenty from high constitution and good strength.', first, second, wherever, str(total_stats[2:])]
-    return class_string
+    class_string = ['The barbarian benefits aplenty from high constitution and good strength.', first, second, wherever, str(total_stats[2:])] 
   elif character_class == "Bard":
     first = "Charisma: " + str(total_stats[0])
     second = "Intelligence: " + str(total_stats[1])
     class_string = ['The bard benefits aplenty from high charisma and good intelligence.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Cleric":
     first = "Wisdom: " + str(total_stats[0])
     second = "Charisma: " + str(total_stats[1])
     class_string = ['The cleric benefits aplenty from high wisdom and good charisma.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Druid":
     first = "Wisdom: " + str(total_stats[0])
     second = "Dexterity: " + str(total_stats[1])
     class_string = ['The druid benefits aplenty from high wisdom and good dexterity.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Fighter":
     first = "Strength: " + str(total_stats[0])
     second = "Constitution: " + str(total_stats[1])
     class_string = ['The fighter benefits aplenty from high strength and good constitution.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Monk":
     first = "Dexterity: " + str(total_stats[0])
     second = "Wisdom: " + str(total_stats[1])
     class_string = ['The monk benefits aplenty from high dexterity and good wisdom.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Paladin":
     first = "Charisma: " + str(total_stats[0])
     second = "Strength: " + str(total_stats[1])
     class_string = ['The paladin benefits aplenty from high charisma and good strength.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Ranger":
     first = "Charisma: " + str(total_stats[0])
     second = "Strength: " + str(total_stats[1])
     class_string = ['The ranger benefits aplenty from high strength and good dexterity.', first, second, wherever, str(total_stats[2:])]
-    return class_string
   elif character_class == "Rogue":
     first = "Dexterity: " + str(total_stats[0])
     second = "Charisma: " + str(total_stats[1])
     class_string = ['The rogue benefits aplenty from high dexterity and good charisma.', first, second, wherever, str(total_stats[2:])]
-    return class_string
+  else:
+    class_strong = "Error: Invalid input"
+  return class_string
 
 if __name__ == '__main__':
   pass
